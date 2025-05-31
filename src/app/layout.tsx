a74d37968c0c627f2b68a57043f29ef91533cbb0
Zou Minowa <zou.minowa@shoppermediagroup.com>
2025-05-31 16:01:44 +0100

    fix the layout

---
+++ src/app/layout.tsx
@@ -0,0 +1,32 @@
+import { type Metadata, type Viewport } from "next";
+
+import './globals.css'
+
+export const metadata: Metadata = {
+  title: "Daisuke Minowa",
+  description:
+    "The website of Daisuke Minowa, a London-based Japanese artist. Here you find a list of my recent and previous artwork.",
+  keywords:
+    "art, painting, illustration, portfolio, japanese artist, london artist",
+  authors: [{ name: "Zou Minowa", url: "https://github.com/Zomino" }],
+  creator: "Zou Minowa",
+};
+
+export const viewport: Viewport = {
+  width: "device-width",
+  initialScale: 1,
+  maximumScale: 1,
+  viewportFit: "cover",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html className="font-arial" lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
