0b4408a53323a4a3d184efc1fb6c3cd52184dab8
Zou Minowa <zou.minowa@shoppermediagroup.com>
2025-06-08 17:18:49 +0100

    remove old loader components

--- client/src/components/SkeletonLoader/SkeletonLoader.tsx
+++
@@ -1,7 +0,0 @@
-export default function SkeletonLoader({ children, className, ...props }: React.PropsWithChildren<React.HTMLAttributes<HTMLDivElement>>) {
-    return (
-        <div className={`from-dark-gray via-darker-gray to-dark-gray animate-pulse bg-gradient-to-r ${className}`} {...props}>
-            <div className="flex h-full w-full items-center justify-center">{children}</div>
-        </div>
-    );
-}
