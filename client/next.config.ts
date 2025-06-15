972a5bd6dab64a551980de6acb25b079cfb34039
Zou Minowa <zouminowa@gmail.com>
2025-06-15 07:57:57 +0100

    Add logging to debug the hostname issue

--- client/next.config.ts
+++ client/next.config.ts
@@ -3,6 +3,8 @@ import { URL } from 'url';
 
 const strapiBaseUrl = new URL(process.env.STRAPI_BASE_URL || 'http://localhost:1337');
 
+console.log(strapiBaseUrl, strapiBaseUrl.hostname);
+
 const nextConfig: NextConfig = {
     images: {
         remotePatterns: [
