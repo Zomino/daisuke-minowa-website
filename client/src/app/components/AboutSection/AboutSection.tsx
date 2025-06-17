d9f6f518136ac8135a80816e7468e41d703f3aff
Zou Minowa <zouminowa@gmail.com>
2025-06-17 07:59:37 +0100

    Touch up the AboutSection

--- client/src/app/components/AboutSection/AboutSection.tsx
+++ client/src/app/components/AboutSection/AboutSection.tsx
@@ -1,16 +1,12 @@
 import strapiClient from '@lib/strapiClient';
-import { type About } from 'genTypes/about';
+import Image from 'next/image';
+
+import profileImage from '../../../../public/images/profile.jpg';
+
 import { type EducationEntry } from 'genTypes/educationEntry';
 import { type ExhibitionEntry } from 'genTypes/exhibitionEntry';
-// The new implementation only supports blurDataURL as a base64 string, not a URL.
-import Image from 'next/legacy/image';
 
 export default async function AboutSection() {
-    const aboutPromise = strapiClient
-        .single('about')
-        .find({ populate: ['profileImage'] })
-        .then((res) => res.data as About)
-        .catch(console.error);
     const educationEntriesPromise = strapiClient
         .collection('education-entries')
         .find()
@@ -22,30 +18,27 @@ export default async function AboutSection() {
         .then((res) => res.data as ExhibitionEntry[])
         .catch(console.error);
 
-    const [about, educationEntries = [], exhibitionEntries = []] = await Promise.all([
-        aboutPromise,
-        educationEntriesPromise,
-        exhibitionEntriesPromise,
-    ]);
+    const [educationEntries = [], exhibitionEntries = []] = await Promise.all([educationEntriesPromise, exhibitionEntriesPromise]);
 
     return (
-        <section id="about">
+        <section id="about" className="p-5">
             <h2 className="text-center text-2xl tracking-widest uppercase md:text-3xl">About</h2>
-            <Image
-                className="mx-auto mt-10 max-w-70 object-contain"
-                src={about?.profileImage?.url || ''}
-                alt={about?.profileImage?.alternativeText || 'Photo of Daisuke Minowa'}
-                width={about?.profileImage?.width}
-                height={about?.profileImage?.height}
-                placeholder="blur"
-                blurDataURL={about?.profileImage?.formats?.thumbnail?.url || ''}
-                // Responsive image sizes: If the viewport is less than 768px, the image will 100vw; otherwise, it will use 70vw.
-                // These are approximate values to help Next JS optimize the image loading.
-                // Ensure that the sizes roughly correspond to the layout and media queries in the CSS.
-                sizes="(max-width: 768px) 100vw, 70vw"
-            />
-            <p className="mx-auto mt-5 max-w-100 p-5 text-center">{about?.bio}</p>
-            <div className="mt-5 px-5 md:flex md:justify-center md:space-x-5">
+            <div className="relative mx-auto mt-10 w-full max-w-[400px] md:w-1/2 lg:w-1/4">
+                <Image
+                    alt="Photo of Daisuke Minowa" // This is a mandatory prop for Next.js Image.
+                    className="object-contain"
+                    // Make sure the height and width match the original image dimensions.
+                    // The height is necessary for the image to actually be visible.
+                    height={3264}
+                    width={2448}
+                    placeholder="blur" // Use blur placeholder for a smooth loading experience.
+                    src={profileImage} // Pass the imported image directly to the src prop to ensure Next.js optimizes it correctly.
+                />
+                {/* Gradient overlay for top and bottom. */}
+                <div className="pointer-events-none absolute inset-0 z-10 bg-gradient-to-b from-black/50 via-transparent to-black/50" />
+            </div>
+            <p className="mx-auto mt-10 max-w-150 text-center">Born in Tokyo in 1966. Moved to the UK in 1986. Living and working in London.</p>
+            <div className="mt-15 md:flex md:justify-center md:space-x-5">
                 <div className="text-center md:w-1/2 md:text-right">
                     <h3 className="text-xl tracking-widest uppercase">Exhibitions</h3>
                     <ul className="mt-5 space-y-6">
@@ -88,7 +81,7 @@ export default async function AboutSection() {
                                         <time dateTime={entry.year_from.toString()}>{entry.year_from}</time>
                                         {entry.year_to && (
                                             <>
-                                                {/* A non-breaking space followed by an en dash and another non-breaking space */}
+                                                {/* A non-breaking space followed by an en dash and another non-breaking space. */}
                                                 {'\u00A0'}
                                                 {'\u2013'}
                                                 {'\u00A0'}
