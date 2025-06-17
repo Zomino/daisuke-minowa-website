0098bebb03a7d1d961d717dbc1fa4ff35f61e894
Zou Minowa <zouminowa@gmail.com>
2025-06-17 14:05:05 +0100

    Run formatter

--- client/src/genTypes/about.ts
+++ client/src/genTypes/about.ts
@@ -1,12 +1,12 @@
 import { Media } from './media';
 
 export interface About {
-  id?: number;
-  documentId?: string;
-  createdAt?: Date | string;
-  updatedAt?: Date | string;
-  publishedAt?: Date | string;
-  locale?: string | null;
-  profileImage: Media | null;
-  bio: string;
-};
+    id?: number;
+    documentId?: string;
+    createdAt?: Date | string;
+    updatedAt?: Date | string;
+    publishedAt?: Date | string;
+    locale?: string | null;
+    profileImage: Media | null;
+    bio: string;
+}
--- client/src/genTypes/contactInfo.ts
+++ client/src/genTypes/contactInfo.ts
@@ -1,12 +1,10 @@
-
-
 export interface ContactInfo {
-  id?: number;
-  documentId?: string;
-  createdAt?: Date | string;
-  updatedAt?: Date | string;
-  publishedAt?: Date | string;
-  locale?: string | null;
-  email: string;
-  link_instagram?: string;
-};
+    id?: number;
+    documentId?: string;
+    createdAt?: Date | string;
+    updatedAt?: Date | string;
+    publishedAt?: Date | string;
+    locale?: string | null;
+    email: string;
+    link_instagram?: string;
+}
