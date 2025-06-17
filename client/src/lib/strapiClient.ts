789ecc2a107f435d034c12afd36d4de32d8d8b7d
Zou Minowa <zouminowa@gmail.com>
2025-06-17 08:57:14 +0100

    Fix Strapi client to use env vars again

--- client/src/lib/strapiClient.ts
+++ client/src/lib/strapiClient.ts
@@ -1,6 +1,5 @@
 import { strapi } from '@strapi/client';
 
-// This URL has been hardcoded as it had to be hardcoded in the Next JS config anyway.
-const client = strapi({ baseURL: `https://graceful-peace-347db111aa.strapiapp.com/api` });
+const client = strapi({ baseURL: `${process.env.STRAPI_BASE_URL || 'http://localhost:1337'}/api` });
 
 export default client;
