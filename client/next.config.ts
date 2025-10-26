506dea3536ca84eb545668d05339563ca74f1df0
Zou Minowa <zouminowa@gmail.com>
2025-10-26 10:37:35 +0000

    add remote

--- client/next.config.ts
+++ client/next.config.ts
@@ -8,6 +8,10 @@ const nextConfig: NextConfig = {
                 hostname: `${process.env.AWS_BUCKET}.s3.${process.env.AWS_REGION}.amazonaws.com`,
                 pathname: '/**',
             },
+            {
+                protocol: 'https',
+                hostname: 'graceful-peace-347db111aa.media.strapiapp.com',
+            },
         ],
     },
 };
